4871fabcfea23c639866d2d44b33ece363546cc0
Dilshod Sadiev <dilshod.sadiev@gmail.com>
2021-08-21 22:00:15 -0400

    Binary Tree insert and find

---
+++ BinaryTree/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BinaryTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tree = new Tree();
+            tree.Insert(7);
+            tree.Insert(4);
+            tree.Insert(9);
+            tree.Insert(1);
+            tree.Insert(6);
+            tree.Insert(8);
+            tree.Insert(10);
+            tree.TraversePreOrder();
+            tree.TraverseInOrder();
+
+
+            Console.WriteLine("Result:"+tree.Find(55));
+        }
+    }
+}
---
+++ BinaryTree/Tree.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Transactions;
+
+namespace BinaryTree
+{
+    public class Tree
+    {
+        private class Node
+        {
+            public Node(int value)
+            {
+                Value = value;
+            }
+
+            public int? Value { get; set; }
+            public Node LeftChild { get; set; }
+            public Node RightChild { get; set; }
+
+            public override string ToString() {
+                return "Node=" + Value;
+            }
+        }
+
+        private Node Root { get; set; }
+        
+        public void Insert(int val) {
+            var node = new Node(val);
+            if (Root == null) {
+                Root = node;
+                return;
+            }
+            var current = Root;
+
+            while (true)
+            {
+                if (val < current.Value)
+                {
+                    if (current.LeftChild == null) {
+                        current.LeftChild = node;
+                        break;
+                    }
+                    current = current.LeftChild;
+                }
+                else {
+                    if (current.RightChild == null) {
+                        current.RightChild = node;
+                        break;
+                    }
+                    current = current.RightChild;
+                }
+            }
+        }
+
+        public bool Find(int val) {
+            var current = Root;
+            while (current!=null)
+            {
+                if (val == current.Value)
+                    return true;
+                if (val < current.Value)
+                {
+                    current = current.LeftChild;
+                }
+                else {
+                    current = current.RightChild;
+                }
+            }
+            return false;
+        }
+
+        public void TraversePreOrder() {
+            Console.WriteLine("Print pre order traversing");
+            TraversePreOrder(Root);
+        }
+        private void TraversePreOrder(Node root) {
+            //Base condition
+            if (root == null)
+                return;
+            //Root
+            Console.WriteLine(root.Value);
+            //Left
+            TraversePreOrder(root.LeftChild);
+            //Right
+            TraversePreOrder(root.RightChild);
+        }
+        public void TraverseInOrder() {
+            Console.WriteLine("Print in order traversing");
+            TraverseInOrder(Root);
+        }
+        private void TraverseInOrder(Node root) {
+            if (root == null) {
+                return;
+            }
+            //Left
+            TraverseInOrder(root.LeftChild);
+            //Root
+            Console.WriteLine(root.Value);
+            //Right
+            TraverseInOrder(root.RightChild);
+        }
+
+
+    }
+}
